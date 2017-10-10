64e5048ca6412674b7c62ddb1aac961b8c47be8d
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-10-10 02:20:52 -0400

    Self-Portrait

--- sketch.js
+++ sketch.js
@@ -1,22 +1,23 @@
-var pnt = {
-  w: 50,
-  h: 50
-}
-var col = {
-  r: 255,
-  g: 255,
-  b: 255
-}
 function setup() {
-  createCanvas(1250, 600);
+  createCanvas(500, 500);
 }
+
 function draw() {
-  background(0);
-  pnt.w = random(0, 150);
-  pnt.h = random(0, 150);
-  col.r = random(0, 255);
-  col.g = random(0, 255);
-  col.b = random(0, 255);
-  fill(col.r, col.g, col.b);
-  ellipse(mouseX, mouseY, pnt.w, pnt.h);
+  //head
+  ellipse(250, 150, 75, 75);
+  //body
+  rect(237, 187, 25, 100);
+  //arms
+  translate(width/2, height/2);
+  rotate(PI/3.0);
+  rect(-50, -10, 15, 50);
+  
+  rotate(PI/3.1);
+  rect(-48, -43, 15, 50);
+  //legs
+  rotate(PI/2.2);
+  rect(-15, -112, 15, 75);
+  
+  rotate(PI/-5);
+  rect(-5, -113, 15, 75);
 }
