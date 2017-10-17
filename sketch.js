da5cc66aab60a7254fecf9920d579d4c31a9314e
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-10-17 14:59:08 -0400

    /October 17th HW

--- sketch.js
+++ sketch.js
@@ -1,67 +1,41 @@
 var serial; // variable to hold an instance of the serialport library
 var portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
-var dataa;
+var fanData;
+var xPos = 0;
 
 function setup() {
-  
-  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial = new p5.SerialPort();
   serial.on('list', printList); // set a callback function for the serialport list event
   serial.on('connected', serverConnected); // callback for connecting to the server
   serial.on('open', portOpen); // callback for the port opening
   serial.on('data', serialEvent); // callback for when new data arrives
   serial.on('error', serialError); // callback for errors
   serial.on('close', portClose); // callback for the port closing
-
-  //serial.list(); // list the serial ports
-  serial.open(portName); // open a serial port
- 
+  
+  serial.open(portName); //opens serial port
+  
+  createCanvas(400,300);
+  background(255);
 }
 
 function draw() {
-  background(255);
-  stroke(0);
-  textSize(36);
-  text("Values coming over the Serial port: " + dataaa, 20, 50);
+  graphData(fanData);
 }
 
+function graphData(newData) {
+  var yPos = map(newData, 0, 255, 0, height);
+  stroke(0);
+  line(xPos, height, xPos, height - yPos);
+  if(xPos >= width) {
+    xPos = 0;
+    background(255);
+  }
+  else {
+    xPos++;
+  }
+}
 
 function serialEvent() {
-  dataa = serial.readStringUntil("\r\n"); //read bytes
-  
-  if(dataa == "hello") {
-    serial.write('x');
-  }
-  if(dataa !== "hello") {
-    dataa = trim(dataa);
-    dataa = Number(dataa);
-    console.log("Recieved data: " + dataa);
-  }
+  fan = Number(serial.read());
 }
 
-function mousePressed() {
-  serial.write(1);
-  console.log("1")
-}
-
-function serialError(err) {
-  console.log('Something went wrong with the serial port. ' + err);
-}
-
-function portClose() {
-  console.log('The serial port closed.');
-}
-
-function printList(portList) {
-  for (var i = 0; i < portList.length; i++) {
-    console.log(i + " " + portList[i]);
-  }
-}
-
-
-function serverConnected() {
-  console.log('connected to server.');
-}
-
-function portOpen() {
-  console.log('the serial port opened.')
-}
