561df03863777893f98e97143ee4d5285a5e077a
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-11-01 02:02:25 -0400

    Old Phone

--- sketch.js
+++ sketch.js
@@ -1,41 +1,157 @@
-var serial; // variable to hold an instance of the serialport library
-var portName = '/dev/cu.usbmodem1411'; // fill in your serial port name here
-var fanData;
-var xPos = 0;
+var num = "";
+var button = {
+  x: 0,
+  y: 0,
+  w: 50,
+  h: 50,
+  display: function() {
+    fill(153);
+    ellipse(this.x, this.y, 50, 50);
+  },
+  clicked: function() {
+    text("");
+    num = "";
+    textSize(22);
+    text("--- CALLING NOW ---", 148, 80);
+  }
+};
+var nums = {
+  n: 1,
+  x: 0,
+  y: 0,
+  display: function() {
+    textSize(40);
+    fill(0);
+    text(this.n, this.x, this.y);
+  }
+};
 
 function setup() {
-  serial = new p5.SerialPort();
-  serial.on('list', printList); // set a callback function for the serialport list event
-  serial.on('connected', serverConnected); // callback for connecting to the server
-  serial.on('open', portOpen); // callback for the port opening
-  serial.on('data', serialEvent); // callback for when new data arrives
-  serial.on('error', serialError); // callback for errors
-  serial.on('close', portClose); // callback for the port closing
-  
-  serial.open(portName); //opens serial port
-  
-  createCanvas(400,300);
-  background(255);
+  createCanvas(500, 500);
+  background(0);
+  createScreen();
+  button.x = 175;
+  button.y = 175;
+  button.display();
+  nums.x = 164;
+  nums.y = 189;
+  nums.n = 1;
+  nums.display();
+  button.x = 250;
+  button.display();
+  nums.x = 239;
+  nums.n = 2;
+  nums.display();
+  button.x = 325;
+  button.display();
+  nums.x = 314;
+  nums.n = 3;
+  nums.display();
+  button.x = 175;
+  button.y = 250;
+  button.display();
+  nums.x = 164;
+  nums.y = 264;
+  nums.n = 4;
+  nums.display();
+  button.x = 250;
+  button.display();
+  nums.x = 239;
+  nums.n = 5;
+  nums.display();
+  button.x = 325;
+  button.display();
+  nums.x = 314;
+  nums.n = 6;
+  nums.display();
+  button.x = 175;
+  button.y = 325;
+  button.display();
+  nums.x = 164;
+  nums.y = 339;
+  nums.n = 7;
+  nums.display();
+  button.x = 250;
+  button.display();
+  nums.x = 239;
+  nums.n = 8;
+  nums.display();
+  button.x = 325;
+  button.display();
+  nums.x = 314;
+  nums.n = 9;
+  nums.display();
+  button.x = 175;
+  button.y = 400;
+  button.display();
+  nums.x = 167;
+  nums.y = 422;
+  nums.n = '*';
+  nums.display();
+  button.x = 250;
+  button.display();
+  nums.x = 239;
+  nums.y = 415;
+  nums.n = 0;
+  nums.display();
+  button.x = 325;
+  button.display();
+  nums.x = 314;
+  nums.n = "#";
+  nums.display();
 }
 
 function draw() {
-  graphData(fanData);
+  fill(0);
+  textSize(28);
+  text(num, 150, 50);
+  if (keyIsPressed) {
+    if(key == "1") {
+      num = num + "1";
+    }
+    else if(key == "2") {
+      num = num + "2";
+    }
+    else if(key == "3") {
+      num = num + "3";
+    }
+    else if(key == "4") {
+      num = num + "4";
+    }
+    else if(key == "5") {
+      num = num + "5";
+    }
+    else if(key == "6") {
+      num = num + "6";
+    }
+    else if(key == "7") {
+      num = num + "7";
+    }
+    else if(key == "8") {
+      num = num + "8";
+    }
+    else if(key == "9") {
+      num = num + "9";
+    }
+    else if(key == "0") {
+      num = num + "0";
+    }
+    if (num.length == 13 || num.length == 26) {
+      num = num + "\n";
+    }
+  }
 }
 
-function graphData(newData) {
-  var yPos = map(newData, 0, 255, 0, height);
-  stroke(0);
-  line(xPos, height, xPos, height - yPos);
-  if(xPos >= width) {
-    xPos = 0;
-    background(255);
-  }
-  else {
-    xPos++;
-  }
+function createScreen() {
+  fill(255);
+  rect(125, 125, 250, 350, 20);
+  fill(255);
+  rect(125, 15, 250, 120, 20);
+  fill(225);
+  rect(135, 25, 230, 100, 20);
 }
 
-function serialEvent() {
-  fan = Number(serial.read());
+function mousePressed() {
+  button.clicked();
 }
 
