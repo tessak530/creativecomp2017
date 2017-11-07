80756776678ae30d69ed5aa8355426d689d97526
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-11-06 22:52:27 -0500

    Popcorn Popping

--- sketch.js
+++ sketch.js
@@ -1,157 +1,64 @@
-var num = "";
-var button = {
-  x: 0,
-  y: 0,
-  w: 50,
-  h: 50,
-  display: function() {
-    fill(153);
-    ellipse(this.x, this.y, 50, 50);
-  },
-  clicked: function() {
-    text("");
-    num = "";
-    textSize(22);
-    text("--- CALLING NOW ---", 148, 80);
+var kernels = [];
+
+function Kernel(x, y) {
+  this.x = x;
+  this.y = y;
+  this.w = 15;
+  this.h = 15;
+  this.col = color(255, 200, 100);
+  
+  this.display = function() {
+    fill(this.col);
+    ellipse(this.x, this.y, this.w, this.h);
+    noFill();
   }
-};
-var nums = {
-  n: 1,
-  x: 0,
-  y: 0,
-  display: function() {
-    textSize(40);
-    fill(0);
-    text(this.n, this.x, this.y);
+  
+  this.dontMove = function() {
+    this.x = this.x;
+    this.y = this.y;
   }
-};
+  
+  this.clicked = function() {
+    var d = dist(mouseX, mouseY, this.x, this.y);
+    if (d < 24) {
+      this.col = color(255, 249, 206);
+      this.w = this.w * 2;
+      this.h = this.h * 2;
+    }
+  }
+  
+  this.move = function() {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  }
+}
 
 function setup() {
-  createCanvas(500, 500);
-  background(0);
-  createScreen();
-  button.x = 175;
-  button.y = 175;
-  button.display();
-  nums.x = 164;
-  nums.y = 189;
-  nums.n = 1;
-  nums.display();
-  button.x = 250;
-  button.display();
-  nums.x = 239;
-  nums.n = 2;
-  nums.display();
-  button.x = 325;
-  button.display();
-  nums.x = 314;
-  nums.n = 3;
-  nums.display();
-  button.x = 175;
-  button.y = 250;
-  button.display();
-  nums.x = 164;
-  nums.y = 264;
-  nums.n = 4;
-  nums.display();
-  button.x = 250;
-  button.display();
-  nums.x = 239;
-  nums.n = 5;
-  nums.display();
-  button.x = 325;
-  button.display();
-  nums.x = 314;
-  nums.n = 6;
-  nums.display();
-  button.x = 175;
-  button.y = 325;
-  button.display();
-  nums.x = 164;
-  nums.y = 339;
-  nums.n = 7;
-  nums.display();
-  button.x = 250;
-  button.display();
-  nums.x = 239;
-  nums.n = 8;
-  nums.display();
-  button.x = 325;
-  button.display();
-  nums.x = 314;
-  nums.n = 9;
-  nums.display();
-  button.x = 175;
-  button.y = 400;
-  button.display();
-  nums.x = 167;
-  nums.y = 422;
-  nums.n = '*';
-  nums.display();
-  button.x = 250;
-  button.display();
-  nums.x = 239;
-  nums.y = 415;
-  nums.n = 0;
-  nums.display();
-  button.x = 325;
-  button.display();
-  nums.x = 314;
-  nums.n = "#";
-  nums.display();
+  createCanvas(1000, 580);
+  background(255);
+  
+  for(var i = 0; i < 20; i++) {
+    var x = random(250, 750);
+    var y = random(150, 450);
+    kernels.push(new Kernel(x, y));
+  }
 }
 
 function draw() {
-  fill(0);
-  textSize(28);
-  text(num, 150, 50);
-  if (keyIsPressed) {
-    if(key == "1") {
-      num = num + "1";
-    }
-    else if(key == "2") {
-      num = num + "2";
-    }
-    else if(key == "3") {
-      num = num + "3";
-    }
-    else if(key == "4") {
-      num = num + "4";
-    }
-    else if(key == "5") {
-      num = num + "5";
-    }
-    else if(key == "6") {
-      num = num + "6";
-    }
-    else if(key == "7") {
-      num = num + "7";
-    }
-    else if(key == "8") {
-      num = num + "8";
-    }
-    else if(key == "9") {
-      num = num + "9";
-    }
-    else if(key == "0") {
-      num = num + "0";
-    }
-    if (num.length == 13 || num.length == 26) {
-      num = num + "\n";
-    }
+  fill(239, 23, 38);
+  ellipse(500, 300, 1000, 800);
+  fill(214, 17, 30);
+  noStroke();
+  ellipse(500, 300, 350, 300);
+  for(var i = 0; i < kernels.length; i++) {
+    kernels[i].move();
+    kernels[i].display();
   }
 }
 
-function createScreen() {
-  fill(255);
-  rect(125, 125, 250, 350, 20);
-  fill(255);
-  rect(125, 15, 250, 120, 20);
-  fill(225);
-  rect(135, 25, 230, 100, 20);
-}
-
 function mousePressed() {
-  button.clicked();
+  for(var i = 0; i < kernels.length; i++) {
+    kernels[i].dontMove();
+    kernels[i].clicked();
+  }
 }
-
