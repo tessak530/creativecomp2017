02be7b3514274cc8cf65bee30ffd39ef842ef6dd
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-11-14 16:23:46 -0500

    Puppy VIdeo

--- sketch.js
+++ sketch.js
@@ -1,64 +1,34 @@
-var kernels = [];
+//with help from NYU ITP website and David Rios
 
-function Kernel(x, y) {
-  this.x = x;
-  this.y = y;
-  this.w = 15;
-  this.h = 15;
-  this.col = color(255, 200, 100);
-  
-  this.display = function() {
-    fill(this.col);
-    ellipse(this.x, this.y, this.w, this.h);
-    noFill();
-  }
-  
-  this.dontMove = function() {
-    this.x = this.x;
-    this.y = this.y;
-  }
-  
-  this.clicked = function() {
-    var d = dist(mouseX, mouseY, this.x, this.y);
-    if (d < 24) {
-      this.col = color(255, 249, 206);
-      this.w = this.w * 2;
-      this.h = this.h * 2;
-    }
-  }
-  
-  this.move = function() {
-    this.x = this.x + random(-1, 1);
-    this.y = this.y + random(-1, 1);
-  }
-}
+var vid;
+var canvas;
 
 function setup() {
-  createCanvas(1000, 580);
-  background(255);
-  
-  for(var i = 0; i < 20; i++) {
-    var x = random(250, 750);
-    var y = random(150, 450);
-    kernels.push(new Kernel(x, y));
-  }
+  createCanvas(600, 338);
+  vid = createVideo("IMG_0251.MOV");
+  vid.size(600, 338);
+  vid.hide();
+  noStroke();
+  //canvas = createImage(600, 338);
+  //canvas.loadPixels();
+  vid.loop();
+  fill(25);
 }
 
 function draw() {
-  fill(239, 23, 38);
-  ellipse(500, 300, 1000, 800);
-  fill(214, 17, 30);
-  noStroke();
-  ellipse(500, 300, 350, 300);
-  for(var i = 0; i < kernels.length; i++) {
-    kernels[i].move();
-    kernels[i].display();
+  background(255);
+  vid.loadPixels();
+  
+  var stepSize = round(constrain(mouseX / 8, 1, 5));
+  
+  for(var y = 0; y < height; y+=stepSize) {
+    for (var x = 0; x < width; x+=stepSize) {
+      var i = y * width + x;
+      
+      var darkness = (255 - vid.pixels[i * 4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
   }
-}
 
-function mousePressed() {
-  for(var i = 0; i < kernels.length; i++) {
-    kernels[i].dontMove();
-    kernels[i].clicked();
-  }
 }
