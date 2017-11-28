c9f8d51909d853e775647ef417fe03168d41b896
tessak530 <32111599+tessak530@users.noreply.github.com>
2017-11-28 16:24:48 -0700

    Final Project
    revision of my final project code

--- sketch.js
+++ sketch.js
@@ -1,36 +1,24 @@
-//with the help of p5 online library
-
-var song, analyzer, fft;
-
-function preload() {
-  song = loadSound('MeurteScore_1.mp3');
-}
+var logo;
+var structure;
 
 function setup() {
-  analyzer = new p5.Amplitude();
-  analyzer.setInput(song);
+  windowWidth = 414;
+  windowHeight = 628;
+  createCanvas(414, 628);
   
-  fft = new p5.FFT();
-  
-  song.loop();
-  
-  createCanvas(500, 500);
+  logo = loadImage("ASLearn_logo.png");
+  background(163, 205, 224);
+  structure = new basicStructure();
 }
 
 function draw() {
-  background(255);
-  var rms = analyzer.getLevel();
-  var spectrum = fft.analyze();
-  noStroke();
-  
-  for(var i = 0; i < spectrum.length; i++) {
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    
-    rect(x, height, 1, h);
-    fill(i / 2, width / spectrum.length, 0);
-    
-    rotate(PI/x);
-    rect(400, 100, 50, 50);
+  for(var j = 0; j < rects.length; j++) {
+     rects[j].display();
+     words[j].display();
   }
+  structure.display();
+}
+
+function mousePressed() {
+  structure.clicked();
 }
